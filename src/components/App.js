f6e33a4c580105fb383c0e324d24f8f99a79ccfe
bthank <bthankachan@yahoo.com>
2021-09-20 05:55:04 -0400

    Added formatting

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
-
+import VideoDetail from './VideoDetail';
+import VideoItem from './VideoItem';
 
 
 class App extends React.Component {
-    state = { videos: [] };
+    state = { 
+                videos: [],
+                selectedVideo: null
+            };
+
+    componentDidMount() {
+        this.onTermSubmit('Superman');
+    };
 
     onTermSubmit = async (term) => {
         console.log('In SearchBar onTermSubmit  term:',term);
@@ -17,17 +25,35 @@ class App extends React.Component {
         });
 
         console.log('In App onTermSubmit   response: ',response);
-        this.setState({ videos: response.data.items });
+        this.setState({ 
+                        videos: response.data.items,
+                        selectedVideo: response.data.items[0]
+                     });
+    };
+
+    onVideoSelect = (video) => {
+        console.log('From the App.   video=',video);
+        this.setState({ selectedVideo: video });
     };
 
 
     render() {
         return (
+            // 16 available columns wide for semantic-ui grid system
             <div className="ui container">
                 <SearchBar onFormSubmit={ this.onTermSubmit } />
-                <VideoList videos={ this.state.videos } />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={ this.state.videos } />               
+                        </div>
+                    </div>
+                </div>
                 <br />
-                I have { this.state.videos.length } videos.
+                { this.state.videos.length } videos.
             </div>
         );
        
